3c055f7163178e16abca01b319e0d48da62fa26f
GrigoriyDyatlov <151855469+GrigoriyDyatlov@users.noreply.github.com>
2025-03-16 20:35:49 +0300

    Update script_2.sql

--- script_2.sql
+++ script_2.sql
@@ -1,3 +1,8 @@
-select name, surname
-from myHW.PERSONS 
-where city_of_living like 'MOSCOW'
+create table myhw.ORDERS (
+id bigserial primary key, 
+date date, 
+customer_id bigint,
+foreign key (customer_id) references myhw.CUSTOMERS(id),
+product_name varchar(255), 
+amount int
+);
