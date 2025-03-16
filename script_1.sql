a499fe3b5ede7ed437551c5012d024e2bcc9e668
GrigoriyDyatlov <151855469+GrigoriyDyatlov@users.noreply.github.com>
2025-03-16 20:35:30 +0300

    Update script_1.sql

--- script_1.sql
+++ script_1.sql
@@ -1,15 +1,7 @@
-CREATE SCHEMA myHW
-
-CREATE TABLE myHW.PERSONS (
-  name text not null
-  surname text not null
-  age int not null
-  phone_namber bigint not null
-  city_of_living text
-  PRIMARY KEY (name, surname, age)
+CREATE table myhw.CUSTOMERS (
+id bigserial primary key, 
+name varchar(255) not null,
+surname varchar(255) not null,
+age int,
+phone_number varchar(11)
 );
-
-INSERT INTO myHW.PERSONS
-values (Petya, Petrov, 20, 79892929224, MOSCOW),
-(Vasya, Pupkin, 30, 79239483223, Novosibirsk),
-(Ivan, Durak, 28, 79134567678, MOSCOW);
